5b8f9dfcb284936e367dff3f90e6ca6ab66dcf5a
Rasso Hilber <mail@rassohilber.com>
2019-04-27 12:47:34 +0200

    Fix Updates Check

--- rh-acf-frontend-forms.php
+++ rh-acf-frontend-forms.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: RH ACF Frontend Forms
- * Version: 2.0.1
+ * Version: 2.0.2
  * Author: Rasso Hilber
  * Description: Frontend forms for Advanced Custom Fields
  * Author URI: https://rassohilber.com
@@ -11,9 +11,20 @@ namespace RH;
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-if( class_exists('\RH_Bitbucket_Updater') ) {
-  new \RH_Bitbucket_Updater( __FILE__ );
-}
+global $rh_updater_notice_shown;
+add_action('plugins_loaded', function() {
+  if( class_exists('\RH_Bitbucket_Updater') ) {
+    new \RH_Bitbucket_Updater( __FILE__ );
+  } else {
+    add_action('admin_notices', function() {
+      global $rh_updater_notice_shown;
+      if( !$rh_updater_notice_shown && current_user_can('activate_plugins') ) {
+        $rh_updater_notice_shown = true;
+        echo "<div class='notice notice-warning'><p>RH_Updater is not installed. Custom plugins won't be updated.</p></div>";
+      }
+    });
+  }
+});
 
 class RH_ACF_Frontend_Forms {
 
