ee36dca0f45249e97b488d75722053a67ddaa449
Rasso Hilber <mail@rassohilber.com>
2020-01-08 16:34:31 +0100

    new Version

--- rh-acf-frontend-forms.php
+++ rh-acf-frontend-forms.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: RH ACF Frontend Forms
- * Version: 3.0.5
+ * Version: 3.0.6
  * Author: Rasso Hilber
  * Description: Frontend forms for Advanced Custom Fields
  * Author URI: https://rassohilber.com
