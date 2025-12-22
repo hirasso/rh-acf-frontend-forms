80bc5d0a89b1a687274d32f2169e59a9a22a3eeb
Rasso Hilber <mail@rassohilber.com>
2025-12-22 15:26:25 +0100

    Do not run husky in CI

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 export default {
-  "**/*.{js,jsx,mjs,cjs,ts,mts,css,scss}": ["prettier --write"],
+  "assets-src/**/*.{js,jsx,mjs,cjs,ts,mts,css,scss}": ["prettier --write"],
   "**/*.php": [
     "pnpm run format:php",
     // "pnpm run analyse:php" // TODO
