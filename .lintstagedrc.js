bdb571b0f5b27bbc8c1d3a551492b1b98f561351
Rasso Hilber <mail@rassohilber.com>
2025-12-22 17:37:46 +0100

    analyse php on precommit

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,4 @@
 export default {
   "assets-src/**/*.{js,jsx,mjs,cjs,ts,mts,css,scss}": ["prettier --write"],
-  "**/*.php": [
-    "pnpm run format:php",
-    // "pnpm run analyse:php" // TODO
-  ],
+  "**/*.php": ["pnpm run format:php", "pnpm run analyse:php"],
 };
